4f938b265763520a8f5ddd3b06e739faa0c8d2ca
David Yarzebinski <dyarzebinski97@gmail.com>
2023-04-26 23:37:52 -0400

    add prime flex

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import "~/styles/globals.css";
 
 import { type AppType } from "next/app";
