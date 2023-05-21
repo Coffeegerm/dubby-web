a94f8744aae4f0a5b591c3fe7888eccbe6b4a0e2
David Yarzebinski <dyarzebinski97@gmail.com>
2023-05-20 18:22:40 -0700

    create navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,14 @@
+import { useSession } from "next-auth/react";
 import { type PropsWithChildren } from "react";
 
-import { Header } from "./header";
+import { Navbar } from "./navbar";
 
 export const Layout = ({ children }: PropsWithChildren<object>) => {
+  const { data: session } = useSession();
   return (
-    <div className="flex min-h-screen flex-1 flex-col bg-gray-900 text-white">
-      <Header />
-      {children}
+    <div className="flex min-h-screen flex-1 flex-row bg-gray-800 text-white">
+      {session?.user && <Navbar />}
+      <div className="flex flex-1 flex-col">{children}</div>
     </div>
   );
 };
---
+++ src/components/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { Button } from "primereact/button";
+
+export const Navbar = () => {
+  return (
+    <div className="flex bg-gray-900">
+      <nav className="flex flex-1 flex-col flex-wrap justify-between p-5">
+        <div className="flex flex-shrink-0 flex-col gap-6 text-white ">
+          <Link href="/dashboard">
+            <span className="text-2xl font-semibold tracking-tight hover:text-gray-400">
+              Dubby
+            </span>
+          </Link>
+          <div className="flex flex-col gap-3">
+            <Link href="/characters">
+              <p className="text-white hover:text-gray-400">Characters</p>
+            </Link>
+          </div>
+        </div>
+
+        <Button
+          label="Logout"
+          className="self-end"
+          onClick={() => {
+            signOut({
+              callbackUrl: `${window.location.origin}`,
+            }).catch(console.error);
+          }}
+        />
+      </nav>
+    </div>
+  );
+};
