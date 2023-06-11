eac5f3e309e7262d69932eedb0aa8b0c4bb08120
David Yarzebinski <dyarzebinski97@gmail.com>
2023-06-11 13:25:29 -0400

    show sign in or dahsboard button on landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,31 @@
 import { type NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { signIn, useSession } from "next-auth/react";
+import { Button } from "primereact/button";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const { replace } = useRouter();
+
   return (
     <main className="flex flex-1 flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h3 className="text-2xl text-white sm:text-[2rem]">
           You and your friends newest companion in your DnD journeys!
         </h3>
-        <div className="flex flex-col items-center gap-2">
-          <AuthShowcase />
-        </div>
+        <Button
+          onClick={() => {
+            if (session?.user) {
+              replace("/dashboard").catch(console.error);
+            } else {
+              signIn().catch(console.error);
+            }
+          }}
+          label={session?.user ? "Go to Dashboard" : "Sign In"}
+        />
       </div>
     </main>
   );
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-    </div>
-  );
-};
